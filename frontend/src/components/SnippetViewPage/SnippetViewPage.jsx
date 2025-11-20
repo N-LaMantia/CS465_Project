142b4ada227743639f91765ea686a6c4314643d3
Matt Eagan <matthew.eagan@snhu.edu>
2025-11-20 12:42:56 -0500

    Setup For new Snippet View Component
    New files and folder path for this feature

---
+++ frontend/src/components/SnippetViewPage/SnippetViewPage.jsx
@@ -0,0 +1,14 @@
+/**
+ * A page that displays the code for a selected snippet; Allows the user to 
+ * copy code, modify code, reset code, and add a second text window for direct
+ * comparison with another chosen language
+ *
+ * @function SnippetViewPage
+ * @author Matthew Eagan
+ * Contributors: Matthew Eagan,
+ *
+ * @return A page for viewing snippet code
+ */
+export default function SnippetViewPage() {
+  return <div>Component Contents</div>;
+}
