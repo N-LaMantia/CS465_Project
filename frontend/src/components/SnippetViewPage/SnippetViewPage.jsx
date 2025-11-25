5be60ac69db018b1f55f9fa45a68d88b498126c4
Matt Eagan <matthew.eagan@snhu.edu>
2025-11-25 14:12:57 -0500

    Current Working SnippetPage
    UI implemented and copy button now copies text within text field. More work needs to be done

--- frontend/src/components/SnippetViewPage/SnippetViewPage.jsx
+++ frontend/src/components/SnippetViewPage/SnippetViewPage.jsx
@@ -7,7 +7,8 @@
  */
 
 import './SnippetViewPage.css';
-
+import { SettingsIcon, CopyIcon, RefreshIcon, AddIcon } from '../../assets.jsx';
+import { useState } from 'react';
 
 /**
  * A function that copies selected snippet code to the user's system-wide
@@ -40,23 +41,78 @@ async function CopySnippetToClipBoard(snippet) {
  * @return A page for viewing snippet code
  */
 export default function SnippetViewPage() {
-  const sampleSnippet = "Hello! This is code for you to copy! \nPress the " + 
-  "button below to copy"
+  const sampleSnippet = `Hello! This is code for you to copy! \nPress the ` + 
+  `button below to copy`
+
+  const [conf, setConf] = useState("");
+
+  const showConf = (confMessage, duration = 3000) => {
+    setConf(confMessage);
+
+    setTimeout(() => {
+      setConf("");
+    }, duration);
+  };
+
+  let currCode = "";
+
+  const CopyButtonHandler = () => {
+    currCode = document.getElementById('codeArea1');
+    currCode = currCode.value;
+    console.log(currCode);
+    CopySnippetToClipBoard(currCode);
+    showConf("Copied!");
+  };
 
   return (
     <>
+      <title>
+        Snippet
+      </title>
+      <header>
+        <div id='siteLogo'>
+          <b>CSnippy</b>
+        </div>
+        <nav>
+          <ul id='navIcons'>
+            <li className='icon'>
+              {<SettingsIcon/>}
+            </li>
+          </ul>
+        </nav>
+      </header>
       <div id='body'>
+        <b>&lt; All Snippets</b>
         <div id='content'>
-          <p>
+          <select id='language1' className='snippetLanguage'>
+            <option value="c++">C++</option>
+            <option value="python">Python</option>
+            <option value="js">JavaScript</option>
+          </select>
+          <textarea id='codeArea1' className='snippetCode'>
             {sampleSnippet}
-          </p>
-          <button className='copySnippet' onClick={
+          </textarea>
+          <button id='copyButton' className='snippetButton' onClick={
+          () => CopyButtonHandler()}>
+            <CopyIcon/>
+          </button>
+          <button id='refreshButton' className='snippetButton' onClick={
             () => CopySnippetToClipBoard(sampleSnippet)
           }>
-            Copy Snippet
+            <RefreshIcon/>
+          </button>
+          <button id='addButton' className='snippetButton' onClick={
+            () => CopySnippetToClipBoard(sampleSnippet)
+          }>
+            <AddIcon/>
           </button>
         </div>
       </div>
+      {conf && (
+        <div id='confMessage'>
+          {conf}
+        </div>
+      )}
     </>
   );
 }
