ba1f3340be791f55e369803619f3705201a0980a
Nicholas LaMantia <134966806+N-LaMantia@users.noreply.github.com>
2025-12-02 13:27:38 -0500

    Revert "Updated start command for render.com and changed proxy to point there…"

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "https://cs465-project.onrender.com",
+        target: "http://localhost:8000",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, "/api"), // optional
       },
