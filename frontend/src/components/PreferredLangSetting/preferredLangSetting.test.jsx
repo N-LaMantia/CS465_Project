0749e1d7fea1b88c14cf762a0039eb2065375cc8
Nicholas LaMantia <nicholas.lamantia@snhu.edu>
2025-12-11 15:33:02 -0500

    hey

--- frontend/src/components/PreferredLangSetting/preferredLangSetting.test.jsx
+++ frontend/src/components/PreferredLangSetting/preferredLangSetting.test.jsx
@@ -51,5 +51,4 @@ test("Renders Preferred Language Setting component", async () => {
     expect(screen.getByRole('button', { name: /Preferred Language:/i })).not.toHaveTextContent("Loading..."); //not loading
     expect(screen.queryByText(/Error:/i)).not.toBeInTheDocument(); //no error
 
-
 });
